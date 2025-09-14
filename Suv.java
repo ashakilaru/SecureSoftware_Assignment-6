caad3f461ce10b6c14cbc430015cf6f97f0dc609
ashakilaru <akilaru43051@ucumberlands.edu>
2025-09-14 15:05:36 -0500

    Create Suv.java

---
+++ Suv.java
@@ -0,0 +1,11 @@
+class SUV extends Vehicle {
+    @Override
+    public void forward() {
+        System.out.println("SUV drives forward with powerful torque.");
+    }
+
+    @Override
+    public void reverse() {
+        System.out.println("SUV reverses with stability control.");
+    }
+}
